a5acde769005279bd4e61aeeb0e4c7c17871f561
yasmim russi mariano <yasmimrussi@gmail.com>
2026-03-18 10:34:30 -0300

    funcionando

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,7 +2,7 @@ const { criarLivro } = require('../services/livroService');
 
 const criar = async (req, res) => {
     const { titulo, autor } = req.body;
-
+    
     if (!titulo || !autor) return res.status(400)
         .json({ erro: 'titulo e autor são obrigatórios'})
 
